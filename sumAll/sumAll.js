5150d7f8724e119efde043a84a1313b53db15732
Ceca Cora <hello@cecacora.com>
2021-01-06 02:32:29 +0800

    Completing Exercise 5!

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function (int1, int2) {
+  if (Number.isInteger(int1) === false || Number.isInteger(int2) === false) {
+    return 'ERROR';
+  } else if (int1 < 0 || int2 < 0) {
+    return 'ERROR';
+  } else {
+    return ((int1 + int2) / 2) * (Math.abs(int2 - int1) + 1);
+  }
+};
 
-}
-
-module.exports = sumAll
+module.exports = sumAll;
