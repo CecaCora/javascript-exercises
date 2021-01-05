b2c1b1dff1ec3c85a4356ca1ec47e95dd15246ca
Ceca Cora <hello@cecacora.com>
2021-01-05 18:35:37 +0800

    Completing Exercise 3!

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let resultString = '';
 
-}
+  for (let i = string.length - 1; i >= 0; i--) {
+    resultString = resultString + string[i];
+  }
 
-module.exports = reverseString
+  return resultString;
+};
+
+module.exports = reverseString;
