8b419722e3a42683e78edba51d1b7295cf6e491d
Ceca Cora <hello@cecacora.com>
2021-01-06 02:58:27 +0800

    Completing Exercise 7!

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,14 @@
-const ftoc = function() {
+const ftoc = function (fTemp) {
+  const celsius = ((fTemp - 32) * 5) / 9;
+  return Math.round(celsius * 10) / 10;
+};
 
-}
-
-const ctof = function() {
-
-}
+const ctof = function (cTemp) {
+  const fahrenheit = (cTemp * 9) / 5 + 32;
+  return Math.round(fahrenheit * 10) / 10;
+};
 
 module.exports = {
   ftoc,
-  ctof
-}
+  ctof,
+};
