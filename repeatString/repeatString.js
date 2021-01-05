47326b8ac2b09bdde3e25e906fe6c8322586c897
Ceca Cora <hello@cecacora.com>
2021-01-05 18:10:34 +0800

    Completing Exercise 2!

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, amount) {
+  let resultString = '';
 
-}
+  if (amount < 0) {
+    return 'ERROR';
+  }
 
-module.exports = repeatString
+  for (let i = 0; i < amount; i++) {
+    resultString = resultString + string;
+  }
+
+  return resultString;
+};
+
+module.exports = repeatString;
