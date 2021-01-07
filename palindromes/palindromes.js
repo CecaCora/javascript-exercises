8a819d21ffd542e0c3fcb1edc26bfa0a606bc6c6
Ceca Cora <hello@cecacora.com>
2021-01-07 08:36:08 +0800

    Fundamentals Part 5 palindromes exercise done!

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function() {
+const palindromes = function (string) {
+  let input = string.toLowerCase().replace(/[^a-z]/g, '');
+  for (let i = 0; i < input.length / 2; i++) {
+    if (input[i] !== input[input.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+};
 
-}
-
-module.exports = palindromes
+module.exports = palindromes;
