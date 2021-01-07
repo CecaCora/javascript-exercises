25254a9a475d85d804632b44c27458bebbb22b42
Ceca Cora <hello@cecacora.com>
2021-01-07 10:01:54 +0800

    Fundamentals Part 5 caesar exercise done!

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,25 @@
-const caesar = function() {
+const caesar = function (string, shift) {
+  let newString = '';
 
-}
+  while (shift < 0) {
+    shift += 26;
+  }
 
-module.exports = caesar
+  for (let i = 0; i < string.length; i++) {
+    if (string[i].match(/[a-z]/i)) {
+      charCode = string[i].charCodeAt(0);
+
+      if (65 <= charCode && charCode <= 90) {
+        newString += String.fromCharCode(((charCode - 65 + shift) % 26) + 65);
+      } else if (97 <= charCode && charCode <= 122) {
+        newString += String.fromCharCode(((charCode - 97 + shift) % 26) + 97);
+      }
+    } else {
+      newString += string[i];
+    }
+  }
+
+  return newString;
+};
+
+module.exports = caesar;
