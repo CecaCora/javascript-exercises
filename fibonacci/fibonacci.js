8861e41dfc5acb72331e40026ca85871fe21ba3c
Ceca Cora <hello@cecacora.com>
2021-01-07 10:25:59 +0800

    Fundamentals Part 5 fibonacci exercise done!

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) {
+    return 'OOPS';
+  }
 
-}
+  if (n === 0) {
+    return 0;
+  }
 
-module.exports = fibonacci
+  let fib = 1;
+  let previousFib = 0;
+
+  for (let i = 1; i < n; i++) {
+    let currentFib = fib;
+    fib = fib + previousFib;
+    previousFib = currentFib;
+  }
+
+  return fib;
+};
+
+module.exports = fibonacci;
