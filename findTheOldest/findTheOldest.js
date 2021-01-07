adcc47afc47d670e0e2ea8ad8b3be2a0dad52b5b
Ceca Cora <hello@cecacora.com>
2021-01-07 11:00:37 +0800

    Fundamentals Part 5 findTheOldest exercise done!

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-let findTheOldest = function() {
+let findTheOldest = function (people) {
+  currentYear = new Date().getFullYear();
 
-}
+  sortOldest = people.sort((a, b) => {
+    let personA = (a?.yearOfDeath || currentYear) - a.yearOfBirth;
+    let personB = (b?.yearOfDeath || currentYear) - b.yearOfBirth;
 
-module.exports = findTheOldest
+    return personA > personB ? -1 : 1;
+  });
+
+  return sortOldest[0];
+};
+
+module.exports = findTheOldest;
